e815512a19624f3e94cda6c663c9f1d9840fac34
atepmrz <atepmichael21@gmail.com>
2025-07-03 13:24:11 +0700

    rev-fix

--- resources/views/main/partial/partPromosi.blade.php
+++ resources/views/main/partial/partPromosi.blade.php
@@ -4,39 +4,51 @@
     }
 </style>
 
-<!-- Promosi -->
+@php
+    $promoData = [
+        1 => 'Pisang Cavendish',
+        2 => 'Apel Fuji, Enoki Mushroom, Pear, Tenderlion Slice, Sirloin Slice',
+        3 => 'Nutella',
+        4 => 'Egg Omega, Egg Daily Fresh',
+        5 => 'Apel Pacific Rose, Queen, Apel Royal Gala',
+        6 => 'Beef Tenderloin, Beef Shortplate',
+    ];
+
+    $promoFinal = [];
+    foreach ($promoData as $id => $nama) {
+        $path = 'img/promo/items/jsm' . $id;
+        foreach (['.jpg', '.jpeg', '.png'] as $ext) {
+            if (file_exists(public_path($path . $ext))) {
+                $promoFinal[] = [
+                    'id' => $id,
+                    'nama' => $nama,
+                    'gambar' => asset($path . $ext)
+                ];
+                break;
+            }
+        }
+    }
+@endphp
+
 <div class="container-fluid pt-0 my-3 px-0">
-    <div class="text-center mx-auto mt-2 wow fadeIn" data-wow-delay="0.1s" style="max-width: 600px">
+    <div class="text-center mx-auto mt-2" style="max-width: 600px">
         <h1 class="display-5 mb-4">Promosi <span class="text-primary">{{ date('F') }}</span></h1>
     </div>
-
-    <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
-        @for ($x = 1; $x <= 6; $x++)
+    <div class="owl-carousel project-carousel">
+        @foreach ($promoFinal as $promo)
             @php
-                $basePath = 'img/promo/items/jsm' . $x;
-                $possibleExtensions = ['.jpg', '.jpeg'];
-                $image = null;
-
-                foreach ($possibleExtensions as $ext) {
-                    $fullPath = public_path($basePath . $ext);
-                    if (file_exists($fullPath)) {
-                        $image = asset($basePath . $ext);
-                        break;
-                    }
-                }
+                $url = ($promo['id'] === 1)
+                    ? url('/layar?produk=' . urlencode($promo['nama'])) 
+                    : url('/layar?produk=' . urlencode($promo['nama']) . '&img=' . urlencode($promo['gambar']));
             @endphp
-
-            @if ($image)
-                <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
-                    <img class="img-fluid rounded2" src="{{ $image }}" alt="Promo {{ $x }}" />
-                    <div class="project-title">
-                        <button class="btn btn-light rounded-4 mb-0 text-primary lead">
-                            Beli Sekarang <i class="bi bi-cart" style="color: rgb(0, 180, 0)"></i>
-                        </button>
-                    </div>
-                </a>
-            @endif
-        @endfor
+            <a class="project-item pb-4 px-3" href="{{ $url }}" target="_blank">
+                <img class="img-fluid rounded2" src="{{ $promo['gambar'] }}" alt="Promo {{ $promo['nama'] }}" />
+                <div class="project-title">
+                    <button class="btn btn-light rounded-4 text-primary lead">
+                        Beli Sekarang <i class="bi bi-cart" style="color: rgb(0,180,0)"></i>
+                    </button>
+                </div>
+            </a>
+        @endforeach
     </div>
 </div>
-<!-- Project End -->
--- resources/views/main/promosi/proJsm.blade.php
+++ resources/views/main/promosi/proJsm.blade.php
@@ -1,97 +1,82 @@
 @extends('main.master_layout')
 @section('konten')
-<<!-- Page Header Start -->
-<div class="container-fluid page-header py-2 mb-0 wow fadeIn bg-warning" data-wow-delay="0.1s">
-    <div class="container py-5">
-        <h1 class="display-3 text-white animated slideInRight">Promo JSM</h1>
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb animated slideInRight mb-0">
-                <li class="breadcrumb-item">
-                    <a href="{{ url('/') }}" class="text-white">Home</a>
-                </li>
-                <li class="breadcrumb-item">
-                    <a href="{{ url('/promo-jsm') }}" class="text-white">Promosi</a>
-                </li>
-                {{-- <li class="breadcrumb-item active" aria-current="page">Promo JSM</li> --}}
-            </ol>
-        </nav>
+    <div class="container-fluid page-header py-2 mb-0 wow fadeIn bg-warning" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <h1 class="display-3 text-white animated slideInRight">Promo JSM</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb animated slideInRight mb-0">
+                    <li class="breadcrumb-item"><a href="{{ url('/') }}" class="text-white">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ url('/promo-jsm') }}" class="text-white">Promosi</a></li>
+                </ol>
+            </nav>
+        </div>
     </div>
-</div>
-<!-- Page Header End -->
 
-<section id="voucher" class="container-fluid">
-    <div class="row p-4 g-4">
-        <div class="col-md">
-            <div class="wow fadeInUp" data-wow-delay="0.5s">
-                <div class="row gy-4">
-                    <div class="col-12">
-                        <div class="d-flex">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
-                                <i class="fa fa-check text-white"></i>
-                            </div>
-                            <div class="ms-4">
-                                <h4>PROMO JSM (Jumat Sabtu Minggu)</h4>
-                                <span>Akhir pekan ada promo di Prama Borma dan Prama Fresh? Iya, setiap akhir pekan ada
-                                    promo menarik yang super hemat di PROMO JSM (Jumat Sabtu Minggu). Belanja week end
-                                    semakin seru, murah dan pas di kantong. </span>
-                            </div>
-                        </div>
-                        <div class="d-flex mt-3">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
-                                <i class="fa fa-check text-white"></i>
-                            </div>
-                            <div class="ms-4">
-                                <h4>Member Day</h4>
-                                <span>Belanja pakai Member, lebih murah dan banyak untungnya</span>
-                            </div>
-                        </div>
-                        <div class="d-flex mt-3">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-primary">
-                                <i class="fa fa-check text-white"></i>
-                            </div>
-                            <div class="ms-4">
-                                <h4>📅 Berlaku hanya: Jumat – Sabtu – Minggu</h4>
-                                <p>🎉 Diskon Spesial untuk Produk Kebutuhan Harian</p>
-                                <p>🍗 Harga hemat untuk daging, ayam, dan bahan segar</p>
-                                <p>🥩 Harga spesial produk segar & sembako</p>
-                                <p>🧴 Promo menarik produk rumah tangga & perawatan tubuh</p>
-                                <p>🍪 Snack dan minuman favorit keluarga, harga hemat!</p>
-                                <p>📍 Kunjungi Borma Toserba terdekat</p>
-                                <p>🎁 Belanja hemat, isi rumah makin lengkap!</p>
-                            </div>
-                        </div>
-                    </div>
+    <section id="voucher" class="container-fluid">
+        <!-- Info Deskripsi Promo -->
+        <div class="row p-4 g-4">
+            <div class="col-md">
+                <div class="wow fadeInUp" data-wow-delay="0.5s">
+                    <h4 class="mb-3">PROMO JSM (Jumat Sabtu Minggu)</h4>
+                    <ul>
+                        <li>🎉 Diskon spesial kebutuhan harian</li>
+                        <li>🍗 Harga hemat produk segar</li>
+                        <li>🧴 Promo produk rumah tangga & perawatan tubuh</li>
+                        <li>🍪 Snack dan minuman favorit keluarga</li>
+                        <li>📍 Kunjungi Borma Toserba terdekat</li>
+                        <li>🎁 Belanja hemat, isi rumah makin lengkap!</li>
+                    </ul>
                 </div>
             </div>
-        {{-- </div>
-        <div class="col-md wow fadeInUp">
-            <img class="img-fluid" src="{{ asset('img/promo/jsm/jsm1.jpg') }}" alt="Promo JSM"
-                style="border-radius: 15px;">
         </div>
-    </div> --}}
-</section>
+    </section>
 
-<style>
-    .rounded2 {
-        border-radius: 10px;
-    }
-</style>
-<!-- Promosi -->
-<div class="wow fadeInUp container-fluid pt-0 my-3 px-0" data-wow-delay="0.1s">
+    @php
+        $jsmData = [
+            1 => 'Anggur Red Globe Aussie',
+            2 => 'Durian Monthong Lokal',
+            3 => 'Lyche',
+            4 => 'Lengkeng Bangkok Super',
+            5 => 'Anggur Hijau Muscat',
+            6 => 'Pear Century',
+            7 => 'Semangka Baby Black, Semangka Merah',
+            8 => 'Anggur Red Globe China',
+            9 => 'Driscoll s blueberry import',
+        ];
 
-    <div class=" owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
+        $promoJSM = [];
+        foreach ($jsmData as $id => $nama) {
+            $base = 'img/promo/jsm/jsm' . $id;
+            foreach (['.jpg', '.jpeg', '.png'] as $ext) {
+                if (file_exists(public_path($base . $ext))) {
+                    $promoJSM[] = [
+                        'nama' => $nama,
+                        'gambar' => asset($base . $ext),
+                    ];
+                    break;
+                }
+            }
+        }
+    @endphp
 
-        @for ($x = 1; $x <= 8; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
-            <img class="img-fluid rounded2" src="{{ asset('img/promo/jsm/jsm' . $x . '.jpg') }}" alt="" />
-            <div class="project-title">
-                <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
-                        style="color: rgb(0, 180, 0)"></i>
-                </button>
-            </div>
-            </a>
-            @endfor
-
+    <style>
+        .rounded2 {
+            border-radius: 10px;
+        }
+    </style>
+    <div class="container-fluid pt-0 my-3 px-0">
+        <div class="owl-carousel project-carousel">
+            @foreach ($promoJSM as $item)
+                <a class="project-item pb-4 px-3"
+                    href="{{ url('/layar?produk=' . urlencode($item['nama']) . '&img=' . urlencode($item['gambar'])) }}">
+                    <img class="img-fluid rounded2" src="{{ $item['gambar'] }}" alt="{{ $item['nama'] }}" />
+                    <div class="project-title">
+                        <button class="btn btn-light rounded-4 text-primary lead">
+                            Beli Sekarang <i class="bi bi-cart" style="color: rgb(0,180,0)"></i>
+                        </button>
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </div>
-</div>
-<!-- Project End -->
 @endsection
